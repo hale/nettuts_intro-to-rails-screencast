0bde0c8dabd78e4fac8ae071cb2922f410f2f71b
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-02-13 00:20:55 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = 'a2c42b17e8ed41128b772e7b8c8f0534dfd06d52151f858ce6326dbcf7bc39d0850fa6c76e4bd4b683fec8968ab331528664f455bea1b4fa96aa7dfddaa60647'
---
+++ spec/requests/tasks_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'test/unit'
+# require 'minitest'
+
+describe "Tasks" do
+  describe "GET /tasks" do
+    it "display some tasks" do
+	    visit tasks_path
+	    page.should have_content "go to bed"
+	    
+    end
+  end
+end
